e468e97df0021aef8d398746e5d03e3a6eda1580
Johnny Lim <izeye@naver.com>
2023-02-13 20:07:24 +0900

    Add hello world for restdocs-api-spec

---
+++ src/test/java/com/izeye/helloworld/restdocsapispec/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.izeye.helloworld.restdocsapispec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
